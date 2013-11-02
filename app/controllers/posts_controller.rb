f80cec74530e569e29817ab21d6edfa1bdbfd75a
Gary Tsai <gary.m.tsai@gmail.com>
2013-11-02 16:33:17 -0700

    made commments in post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,15 @@
 class PostsController < ApplicationController
 
   def index
+    #This renders the form it's the view (GET) and 
     @posts = Post.all
   end
 
-  def new
+  def new # This will generate the from (POST) - will save to the database
     @post = Post.new
   end
 
-  def create
+  def create #Saves to the Database
    @post = Post.new(params[:post])
    if @post.save
     redirect_to posts_path
@@ -17,11 +18,11 @@ class PostsController < ApplicationController
    end
   end
 
-  def edit
+  def edit #View to user to edit the post
     @post = Post.find(params[:id])
   end
 
-  def update
+  def update #Tells the server to update the form
     @post = Post.find(params[:id])
 
     if @post.update_attributes(params[:post])
@@ -31,17 +32,17 @@ class PostsController < ApplicationController
     end
   end
 
-  def delete
+  def delete #some form/link that says delete to the user to the user
     @post = Post.find(params[:id])
   end
 
-  def destroy
+  def destroy #tells the backend to destroy that post!
     @post = Post.find(params[:id])
     @post.destroy
     redirect_to post_path
   end
 
-  def show
+  def show #Will render all posts in the POST route.
     @posts = Post.all
     redirect_to posts_path
   end
